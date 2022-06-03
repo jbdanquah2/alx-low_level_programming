3f6c3d7a90a821506419d3c3fc3814ef23a394af
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-02 22:56:13 -0700

    print a string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
   /**
    * this program prints a string with puts
    */
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
