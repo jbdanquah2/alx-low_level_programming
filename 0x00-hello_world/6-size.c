d04cdb8f20458be3790333c111dd1b049beef323
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-03 04:18:19 -0700

    print out sizeof c var types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of a char:%ld\n bytes(s)", sizeof(char));
-printf("Size of a int:%ld\n bytes(s)", sizeof(int));
-printf("Size of a long int:%ld\n bytes(s)", sizeof(long int));
-printf("Size of a long long int:%ld\n bytes(s)", sizeof(long long int)); 
-printf("Size of a float:%ld\n bytes(s)", sizeof(float));
+printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+printf("Size of a int:% ld bytes(s)\n", sizeof(int));
+printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int)); 
+printf("Size of a float: %ld bytes(s)\n", sizeof(float));
 return (0);
 }
