b33d29ec7c43e0af24ba02ec12744452d8123c43
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-04 22:43:00 +0000

    make code bety style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -7,21 +6,21 @@
 /* better style doc for function main goes here */
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = (rand() - RAND_MAX) / 2 ;
-if (n > 0 )
-{
-printf("%d is positive\n",n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n",n);
-}
-else
-{
-printf("%d is negative\n",n);
-}
-return (0);
+	srand(time(0));
+	n = (rand() - RAND_MAX) / 2 ;
+	if (n > 0 )
+	{
+		printf("%d is positive\n",n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n",n);
+	}
+	else
+	{
+		printf("%d is negative\n",n);
+	}
+	return (0);
 }
