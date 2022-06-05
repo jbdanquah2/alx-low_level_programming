21ef2ca1f2317e0a9d50f289951829b85e2f343e
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-05 19:00:33 +0000

    check if number of greater or less than 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,16 @@ int main(void)
 	las_digit = n % 10;
 	if (las_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, las_digit);
+		printf("Last digit of %d is %d and is greater than 5", n, las_digit);
 	}
 	else if (las_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, las_digit);
+		printf("Last digit of %d is %d and is 0", n, las_digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, las_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, las_digit);
 	}
+	printf("\n");
 	return (0);
 }
