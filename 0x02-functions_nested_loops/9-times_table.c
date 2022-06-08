a9c11dbfd6a81ac705ab8e72e3b2bac99d6d23a6
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-08 20:53:06 +0000

    retry

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,30 +8,29 @@
  */
 void times_table(void)
 {
-	int i;
-	int  j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			int n = j * i;
-			printf("%d", n);
+			
+			if (n % 10 == n)
+			{
+				printf(" %d", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
 			if (j == 9)
 			{
 				continue;
 			}
 			else
 			{
-				printf(",");
-				if (n % 10 == n)
-				{
-					printf("  ");
-				}
-				else
-				{
-					printf(" ");
-				}
+				printf(", ");
 			}
 		}
 		if (j == 10)
