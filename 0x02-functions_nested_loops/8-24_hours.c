fba156193eefa9309b4e94c83a33805fab03014f
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-08 23:27:03 +0000

    print s time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,10 +8,19 @@
  */
 void jack_bauer(void)
 {
-	int i;
+	int i, j;
 
-	for (i = '00:00'; i <= '23:59'; i++)
+	for (i = 0; i < 24; i++)
 	{
-		_putchar(i + '0');
-	} 
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+	 
 }
