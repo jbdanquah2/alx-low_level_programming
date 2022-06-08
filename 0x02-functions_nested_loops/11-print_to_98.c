3f8539c1196fe326fbc372ad060e02c6bd6c354a
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-08 22:37:39 +0000

    retry

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,32 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		if (n == 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
+			if (n == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
 		}
-		else
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (n == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
 		}
 	}
 	printf("\n");
