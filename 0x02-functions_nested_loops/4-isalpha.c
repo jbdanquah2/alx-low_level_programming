c3fb128a658cf7851d471f44cba172b94bcb8e2c
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-08 16:44:11 +0000

    checks if a char is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks if char is alphabet
+ * @c: the char that needs to be checked
+ *
+ * Return: 1 if true and 0 if false
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);	
+	}
+	return (0);
+}
