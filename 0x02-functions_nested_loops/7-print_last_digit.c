e599752e4c7a6b14e472b0857f13a087631be0f5
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-08 18:29:44 +0000

    retry

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,15 @@
  */
 int print_last_digit(int i)
 {
-	static int last_digit;
+	int last_digit;
 
 	last_digit = i % 10;
 	if (last_digit < 0)
 	{
+		_putchar(last_digit + '0');
 		return (-last_digit);
 	}
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
 
