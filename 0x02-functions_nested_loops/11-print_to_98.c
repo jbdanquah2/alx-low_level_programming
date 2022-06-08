ed0ae1567c0b735e6528b6d87122687cdf84c538
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-08 22:28:20 +0000

    print numbers from n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98
+ * @n: number to print from
+ *
+ * Return: result
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (n == 98)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf("%d, ", i);
+		}
+	}
+}
