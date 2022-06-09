15a3230125d796acc3d82155ce580998f3cebfe7
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-09 11:59:55 +0000

    return mult of two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two numbers
+ * @a: first digit
+ * @b: second digit
+ *
+ * Return: result.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
