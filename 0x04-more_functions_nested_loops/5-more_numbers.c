71a1d01e7dc8a350090e82caf7057e17b038ffde
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-09 12:40:16 +0000

    print number 0 -14 with _puthcar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints nums 0 - 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j / 10 == 0)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
