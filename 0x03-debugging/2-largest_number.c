837bde119a800779b590ce5d232df4f98c44f026
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-10 17:08:12 +0000

    modify if statments

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,21 +13,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && (a > c || b > c))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > a && (b > c || a > c))
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
-	{
-		largest = c;
-	}
 	else
 	{
-		largest = a;
+		largest = c;
 	}
 	return (largest);
 }
