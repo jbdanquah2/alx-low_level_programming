8ae22657f61bf30285af2c7c2ce56b89cf7e4955
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-13 20:27:43 +0000

    prins string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse with _putchar
+ * @s: string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len; 
+
+	len = strlen(s);
+	while(len > -1 )
+	{
+		_putchar(s[len--]);
+	}
+	_putchar('\n');
+}
