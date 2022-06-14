391a1f41a65526dcce4ffe2faec00c61a7e8b0f6
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-14 19:35:05 +0000

    prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,9 +13,9 @@ void puts2(char *str)
 	int len;
 
 	len = strlen(str);
-	for (i = 0; i < len; i += 2)
+	for (i = 0; i < len; i=i + 2)
 	{
-		_putchar("%d",(int)str[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
