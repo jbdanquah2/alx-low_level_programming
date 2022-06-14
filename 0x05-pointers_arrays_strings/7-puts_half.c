6fd262bcb73648a6dae19aa23629845db8898b2c
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-14 20:27:11 +0000

    prints the last n digits of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print half
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len;
+	int halfLen;
+
+	len = strlen(str);
+	if (len % 3 == 0)
+	{
+		halfLen = (len - 1) / 2;
+	}
+	else
+	{
+		halfLen = len / 2;
+	}
+	for (i = halfLen; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
