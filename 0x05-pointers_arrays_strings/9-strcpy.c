56ec1aea4f99e30417e71fe97f53819f339eac31
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-14 22:30:00 +0000

    copies an array using pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copies a string to another
+ * @dest: buffer to be copied to 
+ * @src: string to be copied
+ * Return: an array
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return dest;
+}
