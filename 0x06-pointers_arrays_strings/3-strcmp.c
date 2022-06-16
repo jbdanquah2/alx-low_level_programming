a68e99e08976080c1bbdb3c9ac851456a02a781c
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-16 10:01:36 +0000

    change the algo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int diff = 0;
 
 	for (i = 0; s1[i] != 0 || s2[i] != 0; i++)
 	{
-		diff = s1[i] - s2[i];
-		if (diff != 0)
-			break;
-	}
-	if (diff > 0)
-	{
-		return (15);
-	}
-	if (diff < 0)
-	{
-		return (-15);
+		if (s1[i] != s2[1])
+		{
+			if (s1[i] < s2[i])
+				return (-15);
+			if (s1[i] > s2[i])
+				return (15);
+		}
 	}
 	return (0);
 }
