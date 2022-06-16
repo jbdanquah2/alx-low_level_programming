7aa2065b51d9b0c1db2ee1e606d7d7091343beea
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-16 02:12:01 +0000

    fix issues with loop

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 	char *ptr = dest;
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (ptr);
 }
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#inlcude "main.h"
+#inlcude <string.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+}
