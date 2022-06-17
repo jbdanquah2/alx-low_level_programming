a8eb352a676076b4c69ff2ffd15b376133f4580a
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-17 23:36:05 +0000

    retry

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
  * leet - encodes a string into leet
@@ -8,21 +10,18 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
 	char *ptr = s;
+	int numbs[] = {4, 3, 0, 7, 1};
+	char caps[] = {'A', 'E', 'O', 'T', 'L'};
+	char small[] = {'a', 'e', 'o', 't', 'l'};
 
 	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		if (s[i] == caps[i] || s[i] == small[i])
+		{
+			s[i] = numbs[i];
+		}
 	}
 	return (ptr);
 }
