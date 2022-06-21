ea07b8eca299b317131350bb1cfcfcc9116dbd60
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-21 11:41:22 +0000

    returns pointer to a searched byte

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: bytes to search
+ *
+ * Return: pointer to the byte that matches
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *ptr;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				ptr = &(s[i]);
+				return (ptr);
+			}
+		}
+	}
+	return (NULL);
+}
