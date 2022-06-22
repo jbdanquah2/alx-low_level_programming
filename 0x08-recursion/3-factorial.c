d718c104a2ed80747482fe924e87ae7ecf0374dd
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-22 23:16:18 +0000

    finds the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - finds the factorial of a number
+ * @n: number
+ *
+ * Return: the factorial of a number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
+
