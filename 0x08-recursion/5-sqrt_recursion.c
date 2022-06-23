7e3c41c021243af327d788a628d0719f1fd8802f
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-23 18:30:16 +0000

    finds the square root;

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - finds the natural sqrt of a number
  * find_sqrt - checks
  * @n: number to find sqrt
  * @k: checker
@@ -12,14 +11,20 @@ int find_sqrt(int k, int n)
 {
 	unsigned long next_k = (k + n / k) / 2;
 
-	if (n < 0 || (n % 10 == 2 || n % 10 == 3 || n % 10 == 7 || n % 10 == 8)
+	if (n < 0)
 		return (-1);
 	if (k * k <= n && (k + 1) * (k + 1) > n)
 		return (k);
 	else
 		return (find_sqrt(next_k, n));
 }
+/**
+ * _sqrt_recursion - calls the find_sqrt function
+ * @n: number to find sqrt
+ *
+ * Return: sqrt of a number
+ */
 int _sqrt_recursion(int n)
-{	
+{
 	return (find_sqrt(1, n));
 }
