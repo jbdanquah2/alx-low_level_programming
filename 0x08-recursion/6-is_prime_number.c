69cda2a0a76928519f16c31698412d2ae23ce4b4
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-23 01:18:00 +0000

    checks for a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,13 +14,13 @@ int is_prime_number(int n)
 	}
 	else
 	{
-		if (n % (n / 2 ) == 0)
+		if (n % (n / 2) == 0)
 		{
 			return (0);
 		}
 		else
 		{
-			return (is_prime_number(n));
+			return (is_prime_number(n / 2));
 		}
 	}
 }
