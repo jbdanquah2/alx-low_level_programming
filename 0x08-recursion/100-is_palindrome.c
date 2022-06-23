7e171bce099e1f8a5381c6ef4047c5ea5aac8491
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-23 22:33:32 +0000

    checks if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,19 +2,34 @@
 #include <string.h>
 
 /**
- * is_palindrome - checks if a string is a palindrome
+ * check_if_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ * @begin: start of string
+ * @end: end of string
+ *
+ * Return: 1 or 0;
+ */
+int check_if_palindrome(char *s, int begin, int end)
+{
+	if (begin == end)
+		return (1);
+	if (s[begin] != s[end])
+		return (0);
+	if (begin < end + 1)
+		return (check_if_palindrome(s, begin + 1, end - 1));
+	return (1);
+}
+/**
+ * is_palindrome - calls check_if_palindrome
  * @s: string to check
  *
- * Return: 1 if true or 0 if false
+ * Return: 1 or 0;
  */
 int is_palindrome(char *s)
 {
-	int i, len = strlen(s);
+	int len = strlen(s);
 
-	for (i = 0; i < len / 2; i++)
-	{
-		if (s[i] != s[len - 1 - i])
-			return (0);
-	}
-	return (1);
+	if (len == 0)
+		return (1);
+	return (check_if_palindrome(s, 0, len - 1));
 }
