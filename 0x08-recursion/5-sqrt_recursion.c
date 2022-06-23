34fdb33a559b3de254d75affbefe5dec3286b50a
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-23 18:09:47 +0000

    finds the square root;

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int find_sqrt(int k, int n)
 {
 	unsigned long next_k = (k + n / k) / 2;
 
-	if (n < 0)
+	if (n < 0 || (n % 10 == 2 || n % 10 == 3 || n % 10 == 7 || n % 10 == 8)
 		return (-1);
 	if (k * k <= n && (k + 1) * (k + 1) > n)
 		return (k);
@@ -20,6 +20,6 @@ int find_sqrt(int k, int n)
 		return (find_sqrt(next_k, n));
 }
 int _sqrt_recursion(int n)
-{
+{	
 	return (find_sqrt(1, n));
 }
