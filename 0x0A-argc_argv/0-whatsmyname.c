b8b87bb019c3f0708b74380ae95b591a2052ca9b
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-24 19:55:47 +0000

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argv: array of strings
+ * @argc: count of argv
+ *
+ * Return: nothing
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project focuses on Arguments to main
