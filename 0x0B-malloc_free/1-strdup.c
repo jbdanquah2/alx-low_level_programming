46a503168d4874006668d6c4cb169c27b93282b9
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-06-29 23:25:56 +0000

    fix errors with standard lib

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - returns a pointer to a sup string
@@ -12,11 +13,17 @@ char *_strdup(char *str)
 {
 	char *newstr;
 	int i, j;
+	int len;
 
-	if (str == NULL)
+	len = strlen(str);
+
+	if (str == NULL || len  == 0)
 		return (NULL);
+
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		newstr = (char *)malloc(sizeof(char) * (i + 1));
+	}
 
 	if (newstr == NULL)
 		return (NULL);
