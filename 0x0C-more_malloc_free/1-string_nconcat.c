93ac754cd9c4253e8f98f362bf27b713f999e19c
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-01 22:17:37 +0000

    concats two stings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,31 +10,74 @@
  *
  * Return: pointer shall point to a newly allocated space in memory
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int i, j;
-	int len = strlen(s2);
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-	/**
-	 * if n is greater the length of s2,
-	 * then set n = length of s2
-	 */
-	if ((int)n >= len)
-		n = len;
-	/**
-	 * allocate memoory using malloc
-	 */
-	ptr = malloc(sizeof(char) * strlen(s1) * n);
+	if (s1 == NULL)
+		s1 = "";
 
-	for (i = 0; s1[i]; i++)
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+
+	if (n > len2)
+		n = len2;
+	ptr = malloc((len1 + n + 1) * sizeof(char));
+
+	if (ptr == NULL)
+		return (0);
+
+	for (i = 0; i < len1; i++)
+	{
 		ptr[i] = s1[i];
+	}
 
-	for (j = 0; j <= (int)n; j++)
+	for (j = 0 ; j <= n; j++)
+	{
 		ptr[j + i] = s2[j];
-
-	ptr[j + i - 1] = '\0';
+	}
+	ptr[i + j] = '\0';
 
 	return (ptr);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
