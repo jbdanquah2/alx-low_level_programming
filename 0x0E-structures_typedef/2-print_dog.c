ddfbc3dbf33281f609a6eeec7d5b6d858a5b76be
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-04 16:42:58 +0000

    change algorithm

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,32 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		return;
+	if (d->name)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+
+	if (d->age)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)");
+	}
+
+	if (d->owner)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)");
+	}
 }
