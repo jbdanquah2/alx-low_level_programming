a695fe356b10b87a8340e4a3366cc254acfec361
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-05 21:19:52 +0000

    retry

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name)
+	if (d->name != NULL)
 	{
 		printf("Name: %s\n", d->name);
 	}
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	}
 
-	if (d->age)
+	if (d->age != NULL)
 	{
 		printf("Age: %f\n", d->age);
 	}
@@ -29,7 +29,7 @@ void print_dog(struct dog *d)
 		printf("Age: (nil)");
 	}
 
-	if (d->owner)
+	if (d->owner != NULL)
 	{
 		printf("Owner: %s\n", d->owner);
 	}
