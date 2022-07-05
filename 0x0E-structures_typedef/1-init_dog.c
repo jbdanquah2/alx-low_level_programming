ca060fb1f3c5e334fa5b84a0cf21da928bc2b3d7
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-05 21:17:13 +0000

    retry

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,10 +15,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	struct dog *ptr;
 
 	ptr = d;
-	ptr = malloc(sizeof(ptr));
-	if (ptr == NULL)
-		return;
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	if (d != NULL)
+	{
+		ptr->name = name;
+		ptr->age = age;
+		ptr->owner = owner;
+	}
 }
