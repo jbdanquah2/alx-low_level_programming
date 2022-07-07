2ffd8f441d90fcd0125293f38d21ed88c430c0b8
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-07 18:53:46 +0000

    prints strings with a separator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings passed to the function
+ * @separator: string to be printed between strings
+ * @n: number of strings passed
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	if (separator != NULL)
+	{
+		va_start(ap, n);
+		for (i = 1; i <= n; i++)
+		{
+			str = va_arg(ap, char*);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s", str);
+			if (i != n)
+				printf("%s", separator);
+		}
+		va_end(ap);
+	}
+	printf("\n");
+}
