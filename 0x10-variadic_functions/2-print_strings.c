cf3920a871f91989e1b9ef56859bafb497ccb8f8
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-07 18:55:22 +0000

    prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
-	if (separator != NULL)
+	if (separator != NULL && n != NULL)
 	{
 		va_start(ap, n);
 		for (i = 1; i <= n; i++)
