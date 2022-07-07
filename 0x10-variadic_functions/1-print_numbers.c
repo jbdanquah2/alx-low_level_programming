b8dfa4b5b0e5ed75a938c0fd127decfdfe8d6716
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-07 18:27:13 +0000

    prints passed parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,17 +14,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (separator == NULL)
-		return;
-
-	va_start(ap, n);
-
-	for (i = 1; i <= n; i++)
+	if (separator != NULL)
 	{
-		printf("%d", va_arg(ap, int));
-		if (i != n)
-			printf("%s", separator);
+		va_start(ap, n);
+
+		for (i = 1; i <= n; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (i != n)
+				printf("%s", separator);
+		}
+		va_end(ap);
+		printf("\n");
 	}
-	va_end(ap);
-	printf("\n");
 }
