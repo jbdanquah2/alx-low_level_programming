ee5ddafeceb5a3dbea5a1ce7f83cd42a3a5cd7e8
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-08 00:32:54 +0000

    remove trailing white spaces

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,19 +15,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
-	if (separator != NULL && n != 0)
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(ap, n);
-		for (i = 0; i < n; i++)
-		{
-			str = va_arg(ap, char*);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s", str);
-			if (i != (n - 1) && separator != NULL)
-				printf("%s", separator);
-		}
-		va_end(ap);
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+	va_end(ap);
 	printf("\n");
 }
