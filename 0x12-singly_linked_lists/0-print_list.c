9f54d231dbf96cf08b020b2fd8767f3fcd848b75
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-19 11:37:32 +0000

    prints all elements of list_t lisdt

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: string or list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+		if (ptr->str != NULL)
+			printf("[%u] %s\n",ptr->len, ptr->str);
+		else
+			printf("[0] (nil)\n");
+		ptr = ptr->next;
+	}
+	return (count);
+}
