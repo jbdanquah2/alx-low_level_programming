e7e8f7b105b3f86836de62a0833d4c6b9639bcb7
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-26 18:18:17 +0000

    creates file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,9 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(sizeof(char) * letters + 1);
 	if (buf == NULL)
-	{
 		return (0);
-	}
 
 	if (filename == NULL)
 		return (0);
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: text
+ *
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, ret;
+	int i = 0;
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	while(text_content[i])
+		i++;
+
+	ret = write(fd, text_content, i);
+	if (ret == -1)
+		return (-1);
+
+	close(fd);
+	return (1);	
+}
