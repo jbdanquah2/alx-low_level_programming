d6efae99f9b5db8222befa5d5f66c76f1c307257
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-26 18:21:00 +0000

    remove trailing white spaces

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, ret;
 	int i = 0;
+
 	if (filename == NULL)
 		return (-1);
 
@@ -21,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while(text_content[i])
+	while (text_content[i])
 		i++;
 
 	ret = write(fd, text_content, i);
@@ -29,5 +30,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(fd);
-	return (1);	
+	return (1);
 }
