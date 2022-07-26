f5a77c82e103fde27eef609ed7089718b8f992ed
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-26 16:45:48 +0000

    retry

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,13 +13,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd, ret1, ret2;
 	char *buf;
 
-	buf = malloc(sizeof(char) * letters + 1);
 	if (buf == NULL)
 	{
 		return (0);
 	}
+
+	buf = malloc(sizeof(char) * letters + 1);
+
+	if (filename == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd == -1 || filename == NULL)
+	if (fd == -1)
 		return (0);
 
 	ret1 = read(fd, buf, letters);
