953b6e7680773b6c974229f2d0074d133780a331
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-07-26 19:08:32 +0000

    retry

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,8 +29,9 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (1);
 	}
 	ret = write(fd, text_content, i);
-	if (ret == -1)
+	if (ret < 0)
 		return (-1);
 
+	close(fd);
 	return (1);
 }
