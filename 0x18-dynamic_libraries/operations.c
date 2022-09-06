44e91c152dda3562e167de0b91ea558f20a49159
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-06 19:07:01 +0000

    imports c functions in python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include "header.h"
+
+/**
+ * add - adds to two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a subtraction of two numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a multiplication of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: dividend of two numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - the remainder of dividing two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of a div
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
