21ddda51e952d6b5a5a3dab1de51141453a9935d
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-21 13:16:13 +0000

    bug fixes

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,33 +1,31 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: head
- * @n: int
- *
- * Return: address of new element or NULL
+ * add_nodeint_end - Adds a node at the begining of a listint_t.
+ * @n: data type pointer of struct
+ * @head: data type pointer of pointer the head/next node
+ * Return: elements of the str new_node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *ptr = *head;
-	listint_t *new;
+	listint_t *new_node, *tmp_node;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		free(new);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
 
-	if (ptr)
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
 	{
-		while (ptr->next)
-			ptr = ptr->next;
-		ptr->next = new;
+		*head = new_node;
 	}
 	else
-		*head = new;
-	return (new);
+	{
+		tmp_node = *head;
+		while (tmp_node->next != NULL)
+			tmp_node = tmp_node->next;
+		tmp_node->next = new_node;
+	}
+	return (new_node);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,6 +5,7 @@
  * @head: data type pointer the head/next node
  * Return: 0 elements all free
  */
+
 void free_listint(listint_t *head)
 {
 	while (head != NULL)
