aba701dc551f3ca57630943f405add8c45087d21
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-22 21:12:06 +0000

    adds creat hash table fxn

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - create a hash table
+* @size:  size of the table
+*
+* Return: pointer to the newly created hash table
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *new_tble;
+    unsigned long int i;
+
+    new_tble = malloc(sizeof(hash_table_t));
+    if (new_tble == NULL)
+    {
+        free(new_tble);
+        return (NULL);
+    }
+
+    new_tble->size = size;
+    new_tble->array = malloc(sizeof(hash_node_t *) * size);
+
+    if (new_tble->array)
+    {
+        free(new_tble);
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        new_tble->array[i] = NULL;
+    }
+
+
+    return (new_tble);
+}
