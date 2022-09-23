c888c59333c42390fbfd7b217986197e82f4ea4a
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-23 11:00:11 +0000

    fix bugs

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,28 +9,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_tble;
-    unsigned long int i;
+	hash_table_t *new_tble;
+	unsigned long int i;
 
-    new_tble = malloc(sizeof(hash_table_t));
-    if (new_tble == NULL)
-    {
-        return (NULL);
-    }
+	new_tble = malloc(sizeof(hash_table_t));
+	if (new_tble == NULL)
+	{
+		return (NULL);
+	}
 
-    new_tble->size = size;
-    new_tble->array = malloc(sizeof(hash_node_t *) * size);
+	new_tble->size = size;
+	new_tble->array = malloc(sizeof(hash_node_t *) * size);
 
-    if (new_tble->array)
-    {
-        return (NULL);
-    }
+	if (new_tble->array)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        new_tble->array[i] = NULL;
-    }
+	for (i = 0; i < size; i++)
+	{
+		new_tble->array[i] = NULL;
+	}
 
 
-    return (new_tble);
+	return (new_tble);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: the key
+ * @value: value associated with the key
+ *
+ * Return: 1 on success or 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	size_t size;
+	size = size_of(hash_table_t->siz
+}
