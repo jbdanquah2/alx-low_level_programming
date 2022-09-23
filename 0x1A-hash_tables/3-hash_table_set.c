8e4c1877c9126d9e3262791c326d22ea89181f35
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-23 20:07:39 +0000

    add newline at end of file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,14 +12,14 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i, idx;
-	char *val;
+	char *copy_val;
 	hash_node_t *new_ht;
 
 	if (ht == NULL || key == NULL || *key == 48 || value == NULL)
 		return (0);
 
-	val = strdup(value);
-	if (val == NULL)
+	copy_val = strdup(value);
+	if (copy_val == NULL)
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
@@ -28,23 +28,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = val;
+			ht->array[i]->value = copy_val;
 			return (1);
 		}
 	}
 	new_ht = malloc(sizeof(hash_node_t));
 	if (new_ht == NULL)
 	{
-		free(val);
+		free(copy_val);
 		return (0);
 	}
 	new_ht->key = strdup(key);
 	if (new_ht->key == NULL)
 	{
-		free(ht);
+		free(new_ht);
 		return (0);
 	}
-	new_ht->value = val;
+	new_ht->value = copy_val;
 	new_ht->next = ht->array[idx];
 	ht->array[idx] = new_ht;
 
