09309a67fbc35ecb6dd635c7c62e27adb025c832
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-23 12:13:01 +0000

    bug fixes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,8 +8,45 @@
  *
  * Return: 1 on success or 0 on failure
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	size_t size;
-	size = size_of(hash_table_t->siz
+	unsigned long int i, idx;
+	char *val;
+	hash_node_t *new_ht;
+
+	if (ht == NULL || key == NULL || *key == 48 || value == NULL)
+		return (0);
+
+	val = strdup(value);
+	if (val == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; ++i)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = val;
+			return (1);
+		}
+	}
+	new_ht = malloc(sizeof(hash_node_t));
+	if (new_ht == NULL)
+	{
+		free(val);
+		return (0);
+	}
+	new_ht->key = strdup(key);
+	if (new_ht->key == NULL)
+	{
+		free(ht);
+		return (0);
+	}
+	new_ht->value = val;
+	new_ht->next = ht->array[idx];
+	ht->array[idx] = new_ht;
+
+	return (1);
 }
