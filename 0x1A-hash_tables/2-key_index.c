60d6d7e5616a6abe3ffbcb114c608c0adc35111e
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-09-23 11:21:49 +0000

    add newline at end of file

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,5 +9,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size)
+	unsigned long int res = hash_djb2(key) % size;
+
+	return (res);
 }
